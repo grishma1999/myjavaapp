80ebe48dcaa528d2468c3da8e07004b89cb823c5
Grishma <gmahapurush1199@gmail.com>
2021-08-18 16:25:36 +0530

    "I am adding add method"

--- myjavaapp/src/myjavaapp/HelloWorld.java
+++ myjavaapp/src/myjavaapp/HelloWorld.java
@@ -2,9 +2,13 @@ package myjavaapp;
 
 public class HelloWorld {
 
+	public static void add(int a,int b) {
+		System.out.println(a+b);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		add(5,6);
 	}
 
 }
